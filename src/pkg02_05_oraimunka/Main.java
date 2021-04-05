1b0dc993515996f987c35a2c3b7442baca687e34
Ricsi <alex19950106@gmail.com>
2021-04-05 23:56:20 +0200

    proba feltoltes

---
+++ src/pkg02_05_oraimunka/Main.java
@@ -0,0 +1,30 @@
+package pkg02_05_oraimunka;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 1;
+        int c = 5;
+        int d;
+        
+        if(b < a){
+            if(b < c){
+                d = b*(a+c);
+            }
+            else{
+                d = c*(a+b);
+            }
+        }
+        else{
+            if(a < c){
+                d = a*(b+c);
+            }
+            else{
+                d = c*(a+b);
+            }
+        }
+        System.out.println(d);
+    }
+    
+}
